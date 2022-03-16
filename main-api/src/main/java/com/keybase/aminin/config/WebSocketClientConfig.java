2942f21bf06bc922c03da79e2d2b2991440d4d93
Andrey Minin <ewkjf@mail.ru>
2022-03-16 23:33:36 +0400

    added everything, what i had in a moment.

--- main-api/src/main/java/com/keybase/aminin/config/WebSocketClientConfig.java
+++ main-api/src/main/java/com/keybase/aminin/config/WebSocketClientConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.converter.MappingJackson2MessageConverter;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.messaging.simp.stomp.StompSessionHandler;
+import org.springframework.scheduling.concurrent.ConcurrentTaskScheduler;
 import org.springframework.web.socket.client.WebSocketClient;
 import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -14,28 +15,29 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 import org.springframework.web.socket.messaging.WebSocketStompClient;
 
 @Configuration
-@EnableWebSocketMessageBroker
-public class WebSocketClientConfig implements WebSocketMessageBrokerConfigurer {
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/btcusdt@trade", "/ws/btcusdt@trade", "/ws");
-    }
-
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws");
-        registry.addEndpoint("wss://stream.binance.com:9443/ws");
-    }
+//@EnableWebSocketMessageBroker
+public class WebSocketClientConfig {
+//    @Override
+//    public void configureMessageBroker(MessageBrokerRegistry config) {
+//        config.enableSimpleBroker("/btcusdt@trade", "/ws/btcusdt@trade", "/ws");
+//    }
+//
+//    @Override
+//    public void registerStompEndpoints(StompEndpointRegistry registry) {
+//        registry.addEndpoint("/ws");
+//        registry.addEndpoint("wss://stream.binance.com:9443/ws");
+//    }
 
     @Bean
     public WebSocketClient webSocketClient() {
-        WebSocketClient webSocketClient = new StandardWebSocketClient();
+        return new StandardWebSocketClient();
+    }
+
+    @Bean
+    public WebSocketStompClient webSocketStompClient(WebSocketClient webSocketClient) {
         WebSocketStompClient stompClient = new WebSocketStompClient(webSocketClient);
         stompClient.setMessageConverter(new MappingJackson2MessageConverter());
-
-        String url = "wss://stream.binance.com:9443";
-        StompSessionHandler sessionHandler = new BinanceStompSessionHandler();
-        stompClient.connect(url, sessionHandler);
-        return webSocketClient;
+        stompClient.setTaskScheduler(new ConcurrentTaskScheduler());
+        return  stompClient;
     }
 }
