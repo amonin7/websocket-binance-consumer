b9567acd022794d6c9dad95698aa07907f0d9388
Andrey Minin <ewkjf@mail.ru>
2022-03-21 22:44:31 +0400

    added consuming from several streams and producing EVERYTHING to the topic

--- go-api/go.mod
+++ go-api/go.mod
@@ -2,12 +2,10 @@ module go-api
 
 go 1.17
 
-require (
-	github.com/gorilla/websocket v1.5.0
-	github.com/segmentio/kafka-go v0.4.30
-)
+require github.com/gorilla/websocket v1.5.0
 
 require (
 	github.com/klauspost/compress v1.14.2 // indirect
 	github.com/pierrec/lz4/v4 v4.1.14 // indirect
+	github.com/segmentio/kafka-go v0.4.30 // indirect
 )
--- go-api/main.go
+++ go-api/main.go
@@ -6,10 +6,14 @@ import (
 	"fmt"
 	"github.com/gorilla/websocket"
 	"github.com/segmentio/kafka-go"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
 	"time"
 )
 
-type TradeMessage struct {
+type Trade struct {
 	EventType     string `json:"e"`
 	EventTime     int64  `json:"E"`
 	TradeTime     int64  `json:"T"`
@@ -21,27 +25,82 @@ type TradeMessage struct {
 	ExchangeType  string `json:"X"`
 }
 
+type TradeMessage struct {
+	Stream   string `json:"stream"`
+	OneTrade Trade  `json:"data"`
+}
+
+type SymbolInfo struct {
+	Symbol string `json:"symbol"`
+}
+
+type ExchInfo struct {
+	Symbols []SymbolInfo `json:"symbols"`
+}
+
 var ctx = context.Background()
 
 const (
-	KAFKA_BROKER = "kafka:29092"
-	KAFKA_TOPIC  = "trades"
+	KafkaBroker = "kafka:29092"
+	KafkaTopic  = "trades"
 )
 
-func main() {
-	// wait for components to be initialized
-	time.Sleep(20 * time.Second)
-
-	// initialize the writer with the broker addresses, and the topic
-	w := kafka.Writer{
-		Addr:  kafka.TCP(KAFKA_BROKER),
-		Topic: KAFKA_TOPIC,
-	}
-
-	conn, _, err := websocket.DefaultDialer.Dial("wss://stream.binancefuture.com/ws/btcusdt@trade", nil)
+func getCurrentTradePairs() []SymbolInfo {
+	resp, err := http.Get("https://api.binance.com/api/v3/exchangeInfo")
 	if err != nil {
-		fmt.Println("The error occurred during connecting to the stream")
+		log.Fatalln(err)
 	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	exchInfo := ExchInfo{}
+	err = json.Unmarshal(body, &exchInfo)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	symbols := exchInfo.Symbols
+	for i, el := range symbols {
+		symbols[i] = SymbolInfo{strings.ToLower(el.Symbol)}
+	}
+
+	return symbols
+}
+
+func makeCombinedStreamsUrl(symbols []SymbolInfo) []string {
+	urlPrefix := "/stream?streams="
+	streamPostfix := "@trade"
+	oneUrl := ""
+	var result []string
+	for i, el := range symbols {
+		if i%199 == 0 {
+			if oneUrl != "" {
+				oneUrl = oneUrl[:len(oneUrl)-1]
+				result = append(result, oneUrl)
+			}
+			oneUrl = urlPrefix + el.Symbol + streamPostfix
+		} else {
+			oneUrl += el.Symbol + "@trade/"
+		}
+	}
+	if result[len(result)-1] == oneUrl {
+		return result
+	} else {
+		oneUrl = oneUrl[:len(oneUrl)-1]
+		result = append(result, oneUrl)
+		return result
+	}
+}
+
+func consumeFromBinanceSocket(socketUrl string, messages chan string, w *kafka.Writer) {
+	conn, _, err := websocket.DefaultDialer.Dial("wss://stream.binancefuture.com"+socketUrl, nil)
+	if err != nil {
+		fmt.Println("The error occurred during connecting to the stream, " + err.Error())
+	}
+	defer conn.Close()
 
 	for {
 		tradeMessage := TradeMessage{}
@@ -50,18 +109,38 @@ func main() {
 			fmt.Println(err)
 			return
 		}
-		tmString, err := json.Marshal(tradeMessage)
+		tmString, err := json.Marshal(tradeMessage.OneTrade)
 		fmt.Println(string(tmString))
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
+
 		err = w.WriteMessages(ctx, kafka.Message{
-			Key:   []byte(tradeMessage.Symbol),
+			Key:   []byte(tradeMessage.OneTrade.Symbol),
 			Value: tmString,
 		})
 		if err != nil {
 			panic("could not write message " + err.Error())
 		}
+	}
+}
+
+func main() {
+	// wait for components to be initialized
+	time.Sleep(20 * time.Second)
+
+	w := kafka.Writer{
+		Addr:  kafka.TCP(KafkaBroker),
+		Topic: KafkaTopic,
+	}
+	messages := make(chan string)
+
+	urls := makeCombinedStreamsUrl(getCurrentTradePairs())
+	for _, el := range urls {
+		go consumeFromBinanceSocket(el, messages, &w)
+	}
+	for m := range messages {
+		fmt.Printf("%s\n", m)
 	}
 }
